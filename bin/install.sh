ae2ea05e4bd795262faa2ab7a53dfa6b81f2d2fc
Cayce Balara <caycebalara@gmail.com>
2022-06-14 14:57:20 -0400

    Add install steps

--- bin/install.sh
+++ bin/install.sh
@@ -9,3 +9,11 @@ cp ~/s3backups/examples/credentials.example ~/.aws/credentials
 
 # copy MySQL defaults example file
 cp ~/s3backups/examples/.mysql.defaults.example ~/.mysql.defaults
+
+# make scripts executable
+chmod 700 ~/s3backups/bin/*
+
+# make configuration files user-only
+chmod 600 ~/.aws/*
+chmod 700 ~/.aws
+chmod 600 ~/.mysql.defaults
